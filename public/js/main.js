d8000debe4e28944d571ff9cc2c464eba1f77262
Riya2709 <riyashaw2709@gmail.com>
2021-03-14 15:46:08 +0530

    heroku web hosting

---
+++ public/js/main.js
@@ -0,0 +1,57 @@
+
+
+
+const submitBtn=document.getElementById("submitBtn");
+const cityName=document.getElementById("cityName");
+const data=document.getElementById("data")
+
+const getInfo =async(event)=>{
+   event.preventDefault();
+   //alert("hii")
+   let  cityVal=cityName.value;
+   if(cityVal==""){
+       data.innerText="Plz enter the city name before search"
+   }
+   else{
+       try{
+           let url=`https://us1.locationiq.com/v1/search.php?key=pk.e025b5dbd5e9a727657ec2e670b72311&q=${cityVal}&format=json`;
+          const response= await fetch(url);
+           const arrdata= await response.json();
+           //console.log([arrdata]);
+           //console.log([arrdata][0].results[0].locations[0].latLng.lat);
+           const lat=arrdata[0].lat;
+           const lon=arrdata[0].lon;
+           let url1=`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=3a41da93bd091afab19233d92d639244`;
+           const response1= await fetch(url1);
+           const data1= await response1.json();
+           const arrdata1=[data1]
+           //console.log(arrdata1);
+           const a=arrdata1[0].list[0].main.aqi;
+           if(a==1){
+            data.innerHTML=`<p> The air quality in ${cityVal} is Good having components in ug/m<sup>3</sup> is<br>${JSON.stringify(arrdata1[0].list[0].components)}</p>`
+           }
+           else if(a==2){
+               data.innerHTML=`<p> The air quality in ${cityVal} is Fair having components in ug/m<sup>3</sup> is<br>${JSON.stringify(arrdata1[0].list[0].components)}</p>`
+           }
+           else if(a==3){
+            data.innerHTML=`<p> The air quality in ${cityVal} is Moderate having components in ug/m<sup>3</sup> is<br>${JSON.stringify(arrdata1[0].list[0].components)}</p>`
+           }
+           else if(a==4){
+            data.innerHTML=`<p> The air quality in ${cityVal} is Poor having components in ug/m<sup>3</sup> is<br>${JSON.stringify(arrdata1[0].list[0].components)}</p>`
+           }
+           else if(a==5){
+            data.innerHTML=`<p> The air quality in ${cityVal} is Very Poor having components in ug/m<sup>3</sup> is<br>${JSON.stringify(arrdata1[0].list[0].components)}</p>`
+           }
+           else{
+            data.innerHTML=`<p> The air quality in ${cityVal} having  components in ug/m<sup>3</sup> is<br>${JSON.stringify(arrdata1[0].list[0].components)}</p>`
+           }
+           
+
+       }catch{
+           console.log("error");
+
+       }
+   }
+}
+
+submitBtn.addEventListener('click',getInfo);
---
+++ src/app.js
@@ -0,0 +1,143 @@
+const fetch=require("node-fetch");
+const path=require("path");
+const express=require("express");
+const app=express();
+const hbs=require("hbs");
+const dfff=require("dialogflow-fulfillment");
+const { response } = require("express");
+const port=process.env.PORT || 2000;
+const static_path=path.join(__dirname,"../public")
+
+const templatepath=path.join(__dirname,"../templates/views");
+app.set("view engine","hbs")
+app.set("views",templatepath);
+app.use(express.static(static_path));
+app.get("",(req,res)=>{
+    res.render("index");
+});
+app.post("/webhook",express.json(),(req,res)=>{
+    const agent=new dfff.WebhookClient({
+        request:req,
+        response:res
+    });
+    const getData=async(agent)=>{
+        var cityVal=agent.parameters.city;
+        let url=`https://us1.locationiq.com/v1/search.php?key=pk.e025b5dbd5e9a727657ec2e670b72311&q=${cityVal}&format=json`;
+        const response=await fetch(url);
+        const data= await response.json();
+        const lat=data[0].lat;
+        const lon=data[0].lon;
+        let url2=`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=3a41da93bd091afab19233d92d639244`;
+        const response1=await fetch(url2);
+        const data1=await response1.json();
+        const arrdata1=[data1];
+        const a=arrdata1[0].list[0].main.aqi
+           if(a==1){
+            agent.add(`The air quality in ${cityVal} is Good having components `);
+            var jsonA=arrdata1[0].list[0].components;
+            var jsonP=JSON.parse(JSON.stringify(jsonA));
+            
+
+            for(key in jsonP){
+                
+                    agent.add(key+":"+jsonP[key]);
+                
+
+            }
+           }
+           else if(a==2){
+               agent.add(`The air quality in ${cityVal} is Fair having components`);
+               var jsonA=arrdata1[0].list[0].components;
+               var jsonP=JSON.parse(JSON.stringify(jsonA));
+               
+   
+            
+                   for(key in jsonP){
+                       agent.add(key+":"+jsonP[key]);
+                   }
+   
+               
+           }
+           else if(a==3){
+             agent.add(`The air quality in ${cityVal} is Moderate having components`);
+             var jsonA=arrdata1[0].list[0].components;
+             var jsonP=JSON.parse(JSON.stringify(jsonA));
+             
+ 
+             for(key in jsonP){
+            
+                     agent.add(key+":"+jsonP[key]);
+                 
+ 
+             }
+           }
+           else if(a==4){
+            agent.add(`The air quality in ${cityVal} is Poor having components`);
+            var jsonA=arrdata1[0].list[0].components;
+            var jsonP=JSON.parse(JSON.stringify(jsonA));
+            
+
+            for(key in jsonP){
+                
+                    agent.add(key+":"+jsonP[key]);
+                
+
+            }
+           }
+           else if(a==5){
+            agent.add(`The air quality in ${cityVal} is Very Poor having components`);
+            var jsonA=arrdata1[0].list[0].components;
+            var jsonP=JSON.parse(JSON.stringify(jsonA));
+            
+
+            for(key in jsonP){
+            
+                    agent.add(key+":"+jsonP[key]);
+                
+
+            }
+           }
+           else{
+      // agent.add(`The air quality in ${cityVal} having  components ${JSON.stringify(arrdata1[0].list[0].components)}`);
+           agent.add(`The air quality in ${cityVal} having components`);
+           var jsonA=arrdata1[0].list[0].components;
+           var jsonP=JSON.parse(JSON.stringify(jsonA));
+           
+
+           for(key in jsonP){
+        
+                   agent.add(key+":"+jsonP[key]);
+               
+
+           }
+           }
+           
+
+
+
+
+
+        };
+    
+        
+    
+    
+    intentMap=new Map();
+    intentMap.set("Get airpollution detail",getData);
+    agent.handleRequest(intentMap);
+    
+});
+app.get("*",(req,res)=>{
+    res.render("404err",{
+        err_cmmnt:"opps:( page not found"
+    });
+});app.get("/*",(req,res)=>{
+    res.render("404err",{
+        err_cmmnt:"opps:( page not found"
+    });
+});
+
+
+app.listen(2000,()=>{
+    console.log(`listening to port ${port}`);
+});
